fe892f82fd9d980b259f9fd232fb59c6989b93c5
xiaoliang.cheng <xiaoliang.cheng@idreamsky.com>
2023-03-30 11:46:12 +0800

    s

--- router/frontend_v1.go
+++ router/frontend_v1.go
@@ -2,13 +2,15 @@ package router
 
 import (
 	"market/app/handlers"
+	"market/app/middleware"
 	"market/app/validator"
 
 	"github.com/gin-gonic/gin"
 )
 
 func initFrontV1Apis(g *gin.RouterGroup) {
-	group := g.Group("/v1") // , middleware.CheckApiSecret()
+	g.GET("/v1/asset/:code", (validator.BsValidator{}).VAsset)
+	group := g.Group("/v1", middleware.CheckApiSecret()) //
 	{
 		group.POST("/pay", (validator.BsValidator{}).VApiOrder)
 
@@ -16,8 +18,6 @@ func initFrontV1Apis(g *gin.RouterGroup) {
 
 		group.POST("/sms", (validator.BsValidator{}).VAccountSms)
 		group.POST("/sms-valid", (validator.BsValidator{}).VAccountSmsValid)
-
-		group.GET("/asset/:code", (validator.BsValidator{}).VAsset)
 
 		group.GET("/listen", (&handlers.Listen{}).Listen)
 		group.GET("/listen/:id", (&handlers.Listen{}).ApiListenInfo)
